c1c67178b7c0622fc9319cde0cc0f0ff93be8d7c
Kmacedo1002 <162503501+Kmacedo1002@users.noreply.github.com>
2024-10-16 17:54:20 -0300

    Correcao classe receita
    Correcao da classe receita para correto funcionamento do try catch na main do programa

--- receita.ts
+++ receita.ts
@@ -9,10 +9,10 @@ private _consultaRef:Consulta;
 
 	constructor(textoReceita: string, consultaRef: Consulta) {
 	
+		this._consultaRef = consultaRef; //deve ser uma instancia da classe Consulta, caso contrário nao será permitida a instancia de Receita
 		this._dataEmissao = new Date;// a data será a do dia em que a consulta referente foi realizada
 		this.validarData();
 		this._textoReceita = textoReceita;
-		this._consultaRef = consultaRef; //deve ser uma instancia da classe Consulta, caso contrário nao será permitida a instancia de Receita
 	}
 
 
